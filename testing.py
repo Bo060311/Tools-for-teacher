7e940dfd0d3bbdc0946cd5dfddba541924796148
Bow_Bounce <baoohiodeptrai@gmail.com>
2026-02-08 10:20:15 +0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,14 @@
+# import os
+# print("JSON path:", os.path.abspath("dulieu.json"))
+# a = [1,2,3,4,5,6,7]
+# stbd = 6
+# sb = 4
+# result = []
+# for i in range(sb):
+#     index = (stbd+i)%len(a)
+#     result.append(a[index])
+# print(result)
+# print(result[len(result)-1])
+a = "1"
+a += "2"
+print(a)
